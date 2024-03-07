8e3c672e8e493bfa063ba62f5b8224f3a20cf021
deep-2907 <146731783+deep-2907@users.noreply.github.com>
2024-03-07 13:39:34 +0530

    Add files via upload

---
+++ End_string.java
@@ -0,0 +1,24 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class End_string {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string :");
+		String str=sc.nextLine();
+		System.out.println("Enter the last string :");
+		String str1=sc.nextLine();
+		
+		boolean result=  str.endsWith(str1);
+		
+		  System.out.println(result);
+		
+		sc.close();
+
+	}
+
+}
